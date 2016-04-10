0dfda03b085c0fe82a0a1946655d1b7db2c7d90f
SeoJungHong <hsj0223@gmail.com>
2016-04-11 01:18:09 +0900

    sample update

--- buzzstore-sample/src/com/buzzvil/buzzstore/sample/MainActivity.java
+++ buzzstore-sample/src/com/buzzvil/buzzstore/sample/MainActivity.java
@@ -2,12 +2,9 @@ package com.buzzvil.buzzstore.sample;
 
 import android.app.Activity;
 import android.content.Context;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
-import android.widget.ToggleButton;
 
 import com.buzzvil.buzzstore.sdk.BuzzStore;
 
@@ -29,19 +26,23 @@ public class MainActivity extends Activity {
 
         BuzzStore.setBuzzStoreListener(new BuzzStore.BuzzStoreListener() {
             @Override
-            public String needCall() {
-                // 해당 함수가 호출 되면 해당 유저의 유저토큰이 리턴되어야 한다.
-                // 보안상의 이유로 해당 유저토큰은 서버 투 서버로만 제공되므로 퍼블리셔앱은 반드시
-                // 퍼블리셔 서버를 통해서 버즈스토어로 요청해야 한다. 자세한 부분은 2번 사항 참조.
+            public String OnNeedAPICall() {
+                /**
+                * 해당 함수가 호출되면 퍼블리셔 서버로의 API 콜을 통해 해당 유저의 유저토큰을 전달받아 리턴해야 한다.
+                * 보안상의 이유로 해당 유저토큰은 Server-To-Server로만 제공되므로 퍼블리셔앱은 반드시 퍼블리셔 서버를 통해서 버즈스토어로 요청해야 한다.
+                */
                 return "EXAMPLE_USER_TOKEN";
             }
 
             @Override
             public void onFail() {
-                // 최대 횟수 만큼 SDK 내부에서 유저토큰 벨리데이션이 실패한 경우 이 함수가 호출 된다.
-                // 이 때, 퍼블리셔는 앱단에서 실패 UI 등을 정의해서 처리해야 한다.
-                Toast.makeText(this, "Fail to Load BuzzStore", Toast.LENGTH_LONG).show();
+                /**
+                * 최대 횟수 만큼 SDK 내부에서 Validation 시도를 하였으나 최종 실패한 경우 이 함수가 호출된다.
+                * 이 때, 퍼블리셔는 실패 UI 등을 유저에게 보이도록 처리하는 것이 권장된다.
+                * ex. 서버 장애로 인한 접속 불가 메세지 알림
+                */
             }
+        });
 
         findViewById(R.id.showStoreButton).setOnClickListener(new View.OnClickListener() {
             @Override
